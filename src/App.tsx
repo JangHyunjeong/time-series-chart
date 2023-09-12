c501d1898e535040cc207e2995f6992fd6169562
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-09-12 14:51:03 +0900

    feat: chart 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { TimeSeriesChart } from "./components/TimeSeriesChart"
 function App() {
   return (
     <>
       <h1>chart</h1>
-      <div className="chart-area"></div>
+      <TimeSeriesChart></TimeSeriesChart>
     </>
   )
 }
